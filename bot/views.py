87b3dffecd11a0fdf1d791e900e54f78ad0e5cd1
Everton Sousa <124410618+EvertonsSousa@users.noreply.github.com>
2024-06-27 14:08:50 -0300

    add

--- bot/views.py
+++ bot/views.py
@@ -11,11 +11,47 @@ def reactpagina1(request):
 def reactpagina2(request):
     return render(request,'reactpagina2.html')
 
+def reactpagina3(request):
+    return render(request,'reactpagina3.html')
+
+def reactpagina4(request):
+    return render(request,'reactpagina4.html')
+
+def reactpagina5(request):
+    return render(request,'reactpagina5.html')
+
+def reactpagina6(request):
+    return render(request,'reactpagina6.html')
+
+def reactpagina7(request):
+    return render(request,'reactpagina7.html')
+
 def javascriptpagina1(request):
     return render(request,'javascriptpagina1.html')
 
 def javascriptpagina2(request):
     return render(request,'javascriptpagina2.html')
+
+def javascriptpagina3(request):
+    return render(request,'javascriptpagina3.html')
+
+def javascriptpagina3(request):
+    return render(request,'javascriptpagina3.html')
+
+def javascriptpagina4(request):
+    return render(request,'javascriptpagina4.html')
+
+def javascriptpagina4(request):
+    return render(request,'javascriptpagina4.html')
+
+def javascriptpagina5(request):
+    return render(request,'javascriptpagina5.html')
+
+def javascriptpagina6(request):
+    return render(request,'javascriptpagina6.html')
+
+def javascriptpagina7(request):
+    return render(request,'javascriptpagina7.html')
 
 def htmlpagina1(request):
     return render(request,'htmlpagina1.html')
